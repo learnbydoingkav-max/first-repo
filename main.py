4ac4ad82b53c754845d0ecf6e348762afe9c71ca
learnbydoingkav-max <learnbydoingkav@gmail.com>
2026-05-07 14:29:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,28 @@
 # A Streamlit app that generates tweets using Google's Gemini
 # AI model, orchestrated through LangChain.
 # ============================================================
+import streamlit as st
 from langchain_google_genai import ChatGoogleGenerativeAI
 # from langchain import LLMChain
 from langchain_core.prompts import PromptTemplate
+# Page Configiration 
+st.set_page_config(page_title="Tweet Generator")
+st.header("Tweet generator")
+st.subheader("Generate tweets using Generative AI")
+                   
+#Secret key name:
+GOOGLE_API_KEY
+api_key = st.secrets.get("GOOGLE_API_KEY")
 
-import streamlit as st
+if not api_key:
+    st.error("Please ass GOOGLE_API_KEY to your Streamlit Secrets."
+             st.stop()
+
+# Initialize model and chain
+# Note: Ensure "gemini-3.1-flash-lite-preview" is available in your region
+#Initializing Google's Gemini Model
+gemini_model = ChatGoogleGenerativeAI(model="gemini-3.1-flash-lite-preview", google_api_key=api_key)
+
 
 import os
 
@@ -19,8 +36,7 @@ tweet_template = "Give me {number} tweets on {topic}"
 
 tweet_prompt = PromptTemplate.from_template(tweet_template)
 
-#Initializing Google's Gemini Model
-gemini_model = ChatGoogleGenerativeAI(model="gemini-3.1-flash-lite-preview")
+
 
 #Create LLM chain using the promt template and model
 tweet_chain = tweet_prompt | gemini_model
@@ -28,9 +44,8 @@ tweet_chain = tweet_prompt | gemini_model
 #Example of using the LLM chain
 response = tweet_chain.invoke({"number" : 5, "topic" : "wars in Middle East"})
 
-import streamlit as st
-st.header("Tweet generator")
-st.subheader("Generate tweets using Generative AI")
+
+
 topic = st.text_input("Topic")
 number = st.number_input("Number of tweets", min_value = 1, max_value = 10, value = 1, step = 1)
 if st.button("Generate"):
