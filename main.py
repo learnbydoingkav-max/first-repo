c13aa7f9afc889033892ce351712a75c97aa1e9c
learnbydoingkav-max <learnbydoingkav@gmail.com>
2026-05-07 14:36:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,29 +25,42 @@ if not api_key:
 #Initializing Google's Gemini Model
 gemini_model = ChatGoogleGenerativeAI(model="gemini-3.1-flash-lite-preview", google_api_key=api_key)
 
-
-import os
-
-os.environ['GOOGLE_API_KEY'] = "AIzaSyC5-iqYz_4vn-LlFWgUDjW_zbvMxLa9ZJM"
-
 #create promt template for generating tweets
-
 tweet_template = "Give me {number} tweets on {topic}"
-
 tweet_prompt = PromptTemplate.from_template(tweet_template)
-
-
-
 #Create LLM chain using the promt template and model
 tweet_chain = tweet_prompt | gemini_model
 
-#Example of using the LLM chain
-response = tweet_chain.invoke({"number" : 5, "topic" : "wars in Middle East"})
-
-
-
-topic = st.text_input("Topic")
-number = st.number_input("Number of tweets", min_value = 1, max_value = 10, value = 1, step = 1)
+#UI Logic
+topic = st.text_input("Topic", placeholder="Enter a topic...")
+number = st.number_input("Number of tweets", min_value = 1, max_value = 10, value = 1)
 if st.button("Generate"):
+  if topic:
+    with
+    st.spinner("Generating tweets..."):
+      try:
+        # This only runs when the button is clicked
+       response = tweet_chain.invoke({"number" : number, "topic" : "topic"})
+st.write(response.content)
+except exception as e:
+st.error(f"AI Error: {e}")
+else:
+  st.warning("Please enter a topic first")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     response = tweet_chain.invoke({"number": number, "topic": topic})
-    st.write(response.content)
+    
