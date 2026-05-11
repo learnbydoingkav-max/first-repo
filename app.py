2b8fe0f1cfa56e0b3d2e4dd0faeca42bad62fb60
learnbydoingkav-max <learnbydoingkav@gmail.com>
2026-05-11 19:32:12 +0200

    Refactor to use ChatGoogleGenerativeAI model
    Updated the implementation to use ChatGoogleGenerativeAI for the Gemini model and added error handling for missing API key.

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 import streamlit as st
-import google.generativeai as genai
-from dotenv import load_dotenv
-import os
-
-# Load environment variables
-load_dotenv()
-
-# Configure Gemini API
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-
-# Load Gemini model
-model = genai.GenerativeModel("gemini-1.5-flash")
-
+from langchain_google_genai import ChatGoogleGenerativeAI
+# from langchain import LLMChain
+#from langchain_core.prompts import PromptTemplate
 # Streamlit UI
 st.title("B1 German Tutor 🇩🇪")
 
 question = st.text_input("Ask your German question")
+
+#Secret key name: GOOGLE_API_KEY
+api_key = st.secrets.get("GOOGLE_API_KEY")
+
+if not api_key:
+    st.error("Please ass GOOGLE_API_KEY to your Streamlit Secrets.")
+    st.stop()
+
+# Initialize model and chain
+# Note: Ensure "gemini-3.1-flash-lite-preview" is available in your region
+#Initializing Google's Gemini Model
+gemini_model = ChatGoogleGenerativeAI(model="gemini-3.1-flash-lite-preview", google_api_key=api_key)
 
 if st.button("Submit"):
 
